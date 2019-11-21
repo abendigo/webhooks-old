a9f3ffde6740b9540f4f1ac65ea75bc0767790be
Mark Oosterveld <mark@oosterveld.org>
2019-11-20 19:58:35 -0500

    Moved to routes directory. Version 0.1.1

--- app.mjs
+++ app.mjs
@@ -2,11 +2,10 @@ import bodyParser from 'body-parser';
 import logger from 'pino-http';
 import polka from 'polka';
 
-import { verify, handler as github } from './github';
+import { verify, handler as github } from './routes/github.mjs';
 
 const { handler } = polka()
   .use(logger())
-  // .use(bodyParser.json({ verify: verify }))
   .post('/github/:repo?', bodyParser.json({ verify: verify }), github);
 
 export default handler;
